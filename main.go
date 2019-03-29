dd8b781b0e8e577e13b60cce1784ed42710799e8
Abilio Esteves <abilio.esteves@ibm.com>
2019-03-29 14:28:36 -0300

    added main

--- main.go
+++ main.go
@@ -1,27 +1,26 @@
 package main
 
 import (
-	"encoding/json"
-	"encoding/json"
-	"net/http"
-	"strconv"
-	"time"
-	"path"	
-	"fmt"
-	"os"
-	"strings"	
+	_ "encoding/json"
+	_ "encoding/json"
+	_ "net/http"
+	_ "strconv"
+	_ "time"
+	_ "path"	
+	_ "fmt"
+	_ "os"
+	_ "strings"	
 
-	"github.com/spf13/cobra"
-	"github.com/spf13/pflag"
-	"github.com/spf13/viper"
-	"github.com/sirupsen/logrus"
-	"github.com/gorilla/mux"
-	"github.com/abilioesteves/goh/gohtypes"
-	"github.com/abilioesteves/goh/gohserver"
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
+	_ "github.com/spf13/cobra"
+	_ "github.com/spf13/pflag"
+	_ "github.com/spf13/viper"
+	_ "github.com/sirupsen/logrus"
+	_ "github.com/gorilla/mux"
+	_ "github.com/abilioesteves/goh/gohtypes"
+	_ "github.com/abilioesteves/goh/gohserver"
+	_ "github.com/prometheus/client_golang/prometheus"
+	_ "github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
-
 }
