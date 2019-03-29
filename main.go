c1557c0d664fd5053cb4678ee68a3cdb28029d58
Abilio Esteves <abilio.esteves@ibm.com>
2019-03-29 17:50:50 -0300

    added gohclient

--- main.go
+++ main.go
@@ -2,24 +2,24 @@ package main
 
 import (
 	_ "encoding/json"
-	_ "encoding/json"
-	_ "net/http"
-	_ "strconv"
-	_ "time"
-	_ "path"	
 	_ "fmt"
+	_ "net/http"
 	_ "os"
-	_ "strings"	
+	_ "path"
+	_ "strconv"
+	_ "strings"
+	_ "time"
 
+	_ "github.com/abilioesteves/goh/gohclient"
+	_ "github.com/abilioesteves/goh/gohserver"
+	_ "github.com/abilioesteves/goh/gohtypes"
+	_ "github.com/gorilla/mux"
+	_ "github.com/prometheus/client_golang/prometheus"
+	_ "github.com/prometheus/client_golang/prometheus/promhttp"
+	_ "github.com/sirupsen/logrus"
 	_ "github.com/spf13/cobra"
 	_ "github.com/spf13/pflag"
 	_ "github.com/spf13/viper"
-	_ "github.com/sirupsen/logrus"
-	_ "github.com/gorilla/mux"
-	_ "github.com/abilioesteves/goh/gohtypes"
-	_ "github.com/abilioesteves/goh/gohserver"
-	_ "github.com/prometheus/client_golang/prometheus"
-	_ "github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
