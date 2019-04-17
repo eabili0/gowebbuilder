00fc5a40b9a8e21f888ad492119264015da05bae
Abilio Esteves <abilio.esteves@ibm.com>
2019-04-17 11:36:17 -0300

    to renew cache

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	_ "bytes"
+	_ "context"
+	_ "crypto/tls"
 	_ "encoding/json"
 	_ "fmt"
 	_ "net/http"
@@ -10,24 +13,20 @@ import (
 	_ "strconv"
 	_ "strings"
 	_ "time"
-	_ "context"
-	_ "bytes"
-	_ "crypto/tls"
 
+	_ "github.com/gorilla/mux"
 	_ "github.com/labbsr0x/goh/gohclient"
 	_ "github.com/labbsr0x/goh/gohserver"
 	_ "github.com/labbsr0x/goh/gohtypes"
-	_ "github.com/gorilla/mux"
 	_ "github.com/prometheus/client_golang/prometheus"
 	_ "github.com/prometheus/client_golang/prometheus/promhttp"
 	_ "github.com/sirupsen/logrus"
 	_ "github.com/spf13/cobra"
 	_ "github.com/spf13/pflag"
 	_ "github.com/spf13/viper"
-	
+
 	_ "golang.org/x/oauth2"
 	_ "golang.org/x/oauth2/clientcredentials"
-	
 )
 
 func main() {
